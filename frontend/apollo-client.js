c167ac990e276774055d9efa006939ef49a89e3f
eduairet <hola@eduairet.com>
2022-11-15 11:37:55 -0600

    Apollo JS file

---
+++ frontend/apollo-client.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://api.thegraph.com/subgraphs/name/interplanetaryfonts/fontproject',
+    cache: new InMemoryCache(),
+});
+
+export default client;
