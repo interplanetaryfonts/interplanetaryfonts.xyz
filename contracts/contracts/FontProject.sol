ddd2562f7bdcfac000b421072583bc08cc327c94
JP <jp@animoto.com>
2022-12-07 23:45:31 -0500

    cleanup

--- contracts/contracts/FontProject.sol
+++ contracts/contracts/FontProject.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "hardhat/console.sol";
 import "./InterPlanetaryFontNFT.sol";
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
