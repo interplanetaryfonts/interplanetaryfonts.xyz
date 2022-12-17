818f9cefdeaaa53e0b1745713240631bd96acdcb
JP <jp@animoto.com>
2022-12-16 23:52:13 -0500

    fix

--- frontend/components/UI/NavBar.js
+++ frontend/components/UI/NavBar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
