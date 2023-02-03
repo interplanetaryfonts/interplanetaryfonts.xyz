5198dde0d6310c822fd2729691a06961168af8df
JP <jp@animoto.com>
2023-02-03 06:37:37 -0500

    fix url rewrites for preview environment

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -14,6 +14,15 @@ export const config = {
 };
 
 export default async function middleware(req) {
+  // If in preview environment perform no rewrites
+  // since preview URLs have dynamic subdomains.
+  // Therefore to access app pages you will need
+  // to navigate to /app/<page route here>
+  // or /home/<page route here> for content pages
+  if (process.env.VERCEL_ENV === "preview") {
+    return NextResponse.next();
+  }
+
   const url = req.nextUrl;
 
   // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
@@ -21,11 +30,6 @@ export default async function middleware(req) {
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
-
-  // // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  // if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-  //   return NextResponse.redirect("https://demo.vercel.pub");
-  // }
 
   /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
@@ -35,6 +39,7 @@ export default async function middleware(req) {
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
           .replace(`.interplanetaryfonts.xyz`, "")
+          .replace(`.ipfonts.xyz`, "")
           .replace(`.app-interplanetaryfonts.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
@@ -47,13 +52,17 @@ export default async function middleware(req) {
   // rewrite root application to `/home` folder
   if (
     hostname === "localhost:3000" ||
-    hostname === "app-interplanetaryfonts.vercel.app"
+    hostname === "app-interplanetaryfonts.vercel.app" ||
+    hostname === "interplanetaryfonts.xyz" ||
+    hostname === "ipfonts.xyz"
   ) {
     return NextResponse.rewrite(new URL(`/home${path}`, req.url));
   }
 
+  // Enable if we decide to use a CMS to create static content pages like in
+  // https://github.com/vercel/platforms/tree/60136482d84acd31e60d7004f3cfe20ab05070fb/pages/_sites/%5Bsite%5D
   // rewrite everything else to `/_sites/[site] dynamic route
-  return NextResponse.rewrite(
-    new URL(`/_sites/${currentHost}${path}`, req.url)
-  );
+  // return NextResponse.rewrite(
+  //   new URL(`/_sites/${currentHost}${path}`, req.url)
+  // );
 }
